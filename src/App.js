1c01fdfdd66fca8a3986cbfe63514afc85f258e1
Natali Schers <schersnatali@gmail.com>
2025-10-23 18:46:28 -0300

    feat: create generic components with styled-components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Search } from './screens/Search';
 import { Details } from './screens/Details';
 import { NotFound } from './screens/NotFound';
+import { NormalizeStyles } from './shared/NormalizeStyles';
+import { Header } from './common-components/Header/Header';
+
 export function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Search />} />
-				<Route path="/details/:id" element={<Details />} />
-				<Route path="*" element={<NotFound />} />
-			</Routes>
-		</BrowserRouter>
+		<>
+			<NormalizeStyles />
+			<Header />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Search />} />
+					<Route path="/details/:id" element={<Details />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</BrowserRouter>
+		</>
 	);
 }
--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,3 +1,42 @@
+import { Alert } from '../common-components/Alert/Alert';
+import { Button } from '../common-components/Button/Button';
+import { ButtonLink } from '../common-components/ButtonLink/ButtonLink';
+import { Caption } from '../common-components/Caption/Caption';
+import { Card } from '../common-components/Card/Card';
+import { Description } from '../common-components/Description/Description';
+import { HeadingOne } from '../common-components/HeadingOne/HeadingOne';
+import { HeadingTwo } from '../common-components/HeadingTwo/HeadingTwo';
+import { SearchField } from '../common-components/SearchField/SearchField';
+import { SelectField, Option } from '../common-components/SelectField/SelectField';
+import { Subtitle } from '../common-components/Subtitle/Subtitle';
+
 export function Search() {
-	return <div>Hello, tela de busca!</div>;
+	return (
+		<div>
+			<SearchField placeholder="Digite um nome de herói ou heroína" />
+			<Button ghost={true}>Buscar</Button>
+			<Card>
+				<HeadingOne>Heading One</HeadingOne>
+				<HeadingTwo>Heading Two</HeadingTwo>
+				<div>
+					<Subtitle>Subtitle</Subtitle>
+				</div>
+				<Description>Description</Description>
+				<div>
+					<Caption>Caption</Caption>
+				</div>
+			</Card>
+			<ButtonLink>Ver Mais</ButtonLink>
+			<SelectField placeholder="Nota">
+				<Option>Opção 1</Option>
+			</SelectField>
+			<Alert type="info">Nenhum herói ou heroína encontrado</Alert>
+			<Alert type="success" small={true}>
+				Sua avaliação foi atribuída com sucesso!
+			</Alert>
+			<Alert type="error" small={true}>
+				Ocorreu um erro ao avaliar
+			</Alert>
+		</div>
+	);
 }
