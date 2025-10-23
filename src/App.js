850b5f693c50c039a89aa122e37375fbd7d8d0c9
Natali Schers <schersnatali@gmail.com>
2025-10-22 21:10:42 -0300

    feat: add routing with react-router and create NotFound component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Search } from './screens/Search';
 import { Details } from './screens/Details';
+import { NotFound } from './screens/NotFound';
 export function App() {
 	return (
-		<>
-			<Search />
-			<Details />
-		</>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Search />} />
+				<Route path="/details/:id" element={<Details />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
